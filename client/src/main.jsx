8610a03bd753a0605bc673f2a813784e2d81a2c0
vpk-11 <83803726+vpk-11@users.noreply.github.com>
2025-11-09 19:42:37 -0500

    Error fixing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,27 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import './index.css';
-import App from './App.jsx';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-});
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <Toaster position="top-right" />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
